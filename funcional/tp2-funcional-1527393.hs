6b791bb57d810b0955deb862de95a8df57bdfffe
Christian <christianquisbert@gmail.com>
2017-04-17 02:53:04 -0300

    Modifico tp2-funcional-1527393.hs enter the commit message for your changes. Lines starting

--- funcional/tp2-funcional-1527393.hs
+++ funcional/tp2-funcional-1527393.hs
@@ -6,29 +6,37 @@ import Text.Show.Functions
 data Cliente = Persona {
 	nombre :: String,
 	resistencia :: Int,
-	amigos :: [Cliente]
-} deriving(Eq,Show)
+	amigos :: [Cliente],
+	bebidas :: [Bebida]
+} deriving(Show)
+
+
+type Bebida = Cliente -> Cliente
 
 --- PUNTO 2.3 ---
 rodri = Persona {
 	nombre = "Rodri",
 	resistencia = 55,
-	amigos = []
+	amigos = [],
+	bebidas = [tomaTintico]
 }
 marcos = Persona {
 	nombre = "Marcos",
 	resistencia = 40,
-	amigos = [rodri]
+	amigos = [rodri],
+	bebidas = [tomaKlusener "guinda"]
 }
 cristian = Persona {
 	nombre = "Cristian",
 	resistencia = 2,
-	amigos = []
+	amigos = [],
+	bebidas = [tomaGrog, tomaJarraLoca]
 }
 ana = Persona {
 	nombre = "Ana",
 	resistencia = 120,
-	amigos = [marcos, rodri]
+	amigos = [marcos, rodri],
+	bebidas = []
 }
 
 --- PUNTO 2.4 ---
@@ -48,34 +56,51 @@ reconocerAmigo uno otro
 esMismaPersona pers1 pers2 = nombre pers1 == nombre pers2
 amigoExistente pers1 amigoABuscar = (elem (nombre amigoABuscar).map nombre.amigos) pers1
 
-agregarAmigo (Persona name resistencia listaAmigos) amigoAAgregar = Persona name resistencia (amigoAAgregar : listaAmigos)
+agregarAmigo (Persona name resistencia listaAmigos bebidas) amigoAAgregar = Persona name resistencia (amigoAAgregar : listaAmigos) bebidas
 
 --- PUNTO 5 ---
 
 ----- Grog -----
-tomaGrog (Persona name resistencia amigos) =  Persona name 0 amigos
---tomaGrog persona = persona {resistencia = 0} (FORMA SIMPLIFICADA DE MODIFICAR LAS CARACTERISTICAS DE PERSONA)
+tomaGrog (Persona name resistencia amigos bebidas) =  Persona name 0 amigos (bebidas ++ [tomaGrog])
+
 ------ Jarra loca -----
-tomaJarraLoca (Persona name resist friends) =  (Persona name (resist - 10) (map (disminuirResistencia 10) friends))
+tomaJarraLoca (Persona name resist friends bebidas) =  (Persona name (resist - 10) (map (disminuirResistencia 10) friends) (bebidas ++ [tomaJarraLoca]))
 
 ----- Toma Klusener -----
-tomaKlusener gusto persona = disminuirResistencia (length gusto) persona
+tomaKlusener gusto persona = (agregarKlusener gusto.disminuirResistencia (length gusto)) persona
+
+agregarKlusener gusto (Persona name res amigos bebidas) = Persona name res amigos (bebidas ++ [tomaKlusener gusto])
 
 ----- Toma Tintico ------
-tomaTintico persona = aumentarResistencia ((length.amigos) persona * 5) persona
+tomaTintico persona = (agregarTintico.aumentarResistencia ((length.amigos) persona * 5)) persona
+
+agregarTintico (Persona name res amigos bebidas) =  (Persona name res amigos (bebidas ++ [tomaTintico]))
 
 ----- Toma Soda -----
-tomaSoda fuerza (Persona name resistencia amigos) = Persona (modificoNombre name fuerza) resistencia amigos
+tomaSoda fuerza (Persona name resistencia amigos bebidas) = Persona (modificoNombre name fuerza) resistencia amigos (bebidas ++ [tomaSoda fuerza])
 
 modificoNombre name cantidadErres = "e" ++ replicate cantidadErres 'r' ++ "p" ++ name
 
-disminuirResistencia cant (Persona name resistencia amigos) = Persona name (resistencia - cant) amigos
-aumentarResistencia cant (Persona name resistencia amigos) = Persona name (resistencia + cant) amigos
+disminuirResistencia cant (Persona name resistencia amigos bebidas) = Persona name (resistencia - cant) amigos bebidas
+aumentarResistencia cant (Persona name resistencia amigos bebidas) = Persona name (resistencia + cant) amigos bebidas
 
 --- PUNTO 6 ---
 -- seRescata :: Cliente -> Int -> Cliente
 seRescata persona horas
 	| horas > 3 = aumentarResistencia 200 persona
 	| horas > 0 && horas <= 3 = aumentarResistencia 100 persona
+
+----------------------------------------------------------
+--- EMPIEZA LA SEGUNDA ENTREGA DE DEL TRABAJO PRACTICO ---
+----------------------------------------------------------
+
+---  PUNTO 1-a ---
+-- Se realizaron las modificaciones en la abstracion "Cliente", agregando parámetro "bebidas" --
+-- Se modificaron se agegaron las bebidas tomadas por cada cliente en su nuevo parametro "bebidas" --
+
+--- Punto 1-b ---
+-- C/u de las funciones donde toma una bebida, se modifico para agregar la bebida.
+
+--- Punto 1-c ---
 
 
