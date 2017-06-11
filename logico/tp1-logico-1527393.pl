f0c04627ba0c2cfb62b187f466116494284611bd
christian-quisbert <christianquisbert@gmail.com>
2017-06-10 21:43:01 -0300

    Corrección tp1 - Lógico. Ok.

--- logico/tp1-logico-1527393.pl
+++ logico/tp1-logico-1527393.pl
@@ -5,11 +5,15 @@
 programa(fernando,cobol).
 programa(fernando,visualBasic).
 programa(fernando,java).
+programa(julieta,java).
+programa(marcos,java).
 programa(santiago,ecmaScript).
 programa(santiago,java).
 
 rol(fernando,analistaFuncional).
 rol(andres,projectLeader).
+rol(Persona,programador):-
+	esProgramador(Persona).
 
 esProgramador(Alguien):-
 	programa(Alguien,_).
