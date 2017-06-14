57671cc6e4192642e1f3ccc845116a31be78c2c8
Christian <christianquisbert@gmail.com>
2017-06-14 01:18:54 -0300

    Terminado - TP2 Lógico.

--- logico/tp2-logico-1527393.pl
+++ logico/tp2-logico-1527393.pl
@@ -61,7 +61,58 @@ programaEnProyecto(Proyecto,Persona):-
 bienDefinido(Proyecto):-
 	proyecto(Proyecto),
 	trabaja(Personas,Proyecto),
-	forall(member(Persona,Personas), bienAsignada(Persona,Proyecto)).
-	/*Y si ademas hay 1 solo projectLeader (Usar not)*/
-	
-	
+	forall(member(Persona,Personas), bienAsignada(Persona,Proyecto)),
+	findall(Persona, (member(Persona,Personas), rol(Persona,projectLeader)), ProyectLeaders),
+	length(ProyectLeaders,Cantidad),
+	Cantidad = 1.
+
+%----------------------------------------------%
+%-------------------- TEST --------------------%
+%----------------------------------------------%
+
+:- begin_tests(programadores).
+
+% Test Entrega 2 - Punto 2 Proyectos
+test(lenguajesDeSumatra, nondet) :-
+    proyecto(sumatra,[java,pnet]).%, proyecto(sumatra, net).
+        
+/*test(lenguajesDeSumatraExplicito, set(Lenguajes == [java, pnet])) :-
+	proyecto(sumatra, Lenguajes).
+*/
+test(lenguajesDePrometeusEsSoloCobol/*, set(Lenguajes == [cobol])*/) :-
+    proyecto(prometeus, [cobol]).
+
+test(fernandoTrabajaEnPrometeus,nondet) :-
+    trabaja(Personas, prometeus),
+	member(fernando,Personas).
+
+test(santiagoTrabajaEnPrometeus) :-
+    trabaja(Personas, prometeus),
+	member(santiago,Personas).
+
+test(genteDeSumatra/*, set(Personas == [julieta, marcos, andres])*/) :-
+    trabaja([julieta, marcos, andres], sumatra).
+
+test(trabajadoresDeSumatraBienAsignados, set(Gente == [julieta, marcos, andres])) :-
+    bienAsignada(Gente, sumatra).
+
+test(trabajadoresDePrometeusBienAsignados, set(Gente == [fernando])) :-
+    bienAsignada(Gente, prometeus).
+
+test(trabajadoresBienAsignados, set(Gente == [julieta, marcos, andres, fernando])) :-
+    bienAsignada(Gente, _).
+
+test(proyectosConGenteBienAsignada, set(Proyectos == [prometeus, sumatra])) :-
+    bienAsignada(_, Proyectos).
+
+% Punto 3 - Proyectos bien definidos
+test(proyectosBienDefinidos, set(Proyectos == [sumatra])) :-
+    bienDefinido(Proyectos).
+
+test(prometeusNoEstaBienDefinido):-
+    not(bienDefinido(prometeus)).
+
+test(proyectosMalDefinidos, set(Proyectos == [prometeus])) :-
+    proyecto(Proyectos), not(bienDefinido(Proyectos)).
+
+:- end_tests(programadores).
